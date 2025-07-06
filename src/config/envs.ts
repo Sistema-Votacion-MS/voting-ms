abfbe5377b7fcc8e0cd2025cea98f1ecd6b3b6b4
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-07-05 22:02:59 -0500

    feat: configure NATS microservice and environment variable validation

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -3,14 +3,18 @@ import * as joi from 'joi';
 
 interface EnvVars {
   PORT: number;
+  NATS_SERVERS: string[];
 }
 
 const envSchema = joi.object({
-  PORT: joi.number().required()  
-})
-.unknown(true);
+  PORT: joi.number().required(),
+  NATS_SERVERS: joi.array().items(joi.string()).required(),
+}).unknown(true);
 
-const {error, value} = envSchema.validate(process.env);
+const { error, value } = envSchema.validate({
+  ...process.env,
+  NATS_SERVERS: process.env.NATS_SERVERS?.split(',')
+});
 
 if (error) {
   throw new Error(`Environment variable validation error: ${error.message}`);
@@ -20,4 +24,5 @@ const envVars: EnvVars = value;
 
 export const envs = {
   port: envVars.PORT,
+  natsServers: envVars.NATS_SERVERS,
 };
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { envs } from './config/envs';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(envs.port);
+  const logger = new Logger('ElectionMicroservice');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
+    },
+  );
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  logger.log('Starting Election Microservice...');
+
+  await app.listen();
 }
 bootstrap();
