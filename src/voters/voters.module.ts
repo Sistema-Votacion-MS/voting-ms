6b3efc26a7dfd71d07ba54100fa1e46d5b5e0cbf
Carlo Valdivia Luna <cvaldivialu@unsa.edu.pe>
2025-07-05 22:50:13 -0500

    feat: register ClientsModule for USER_SERVICE and ELECTION_SERVICE in VotersModule and VotesModule

--- src/voters/voters.module.ts
+++ src/voters/voters.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
 import { VotersService } from './voters.service';
 import { VotersController } from './voters.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ELECTION_SERVICE, USER_SERVICE } from 'src/config/services';
+import { envs } from 'src/config/envs';
 
 @Module({
   controllers: [VotersController],
   providers: [VotersService],
   exports: [VotersService],
+  imports: [
+    ClientsModule.register([
+      {
+        name: USER_SERVICE,
+        transport: Transport.NATS,
+        options: {
+          servers: envs.natsServers,
+        },
+      },
+      {
+        name: ELECTION_SERVICE,
+        transport: Transport.NATS,
+        options: {
+          servers: envs.natsServers,
+        },
+      },
+    ]),
+  ]
 })
 export class VotersModule { }
--- src/votes/votes.module.ts
+++ src/votes/votes.module.ts
@@ -2,9 +2,30 @@ import { Module } from '@nestjs/common';
 import { VotesService } from './votes.service';
 import { VotesController } from './votes.controller';
 import { VotersModule } from 'src/voters/voters.module';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ELECTION_SERVICE, USER_SERVICE } from 'src/config/services';
+import { envs } from 'src/config/envs';
 
 @Module({
-  imports: [VotersModule],
+  imports: [
+    VotersModule,
+    ClientsModule.register([
+      {
+        name: USER_SERVICE,
+        transport: Transport.NATS,
+        options: {
+          servers: envs.natsServers,
+        },
+      },
+      {
+        name: ELECTION_SERVICE,
+        transport: Transport.NATS,
+        options: {
+          servers: envs.natsServers,
+        },
+      },
+    ]),
+  ],
   controllers: [VotesController],
   providers: [VotesService],
 })
